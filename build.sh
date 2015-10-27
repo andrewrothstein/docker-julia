eaf1b8a9da2fabe3fc5a041afee5927c329da6dc
Andrew Rothstein <arothste@blackrock.com>
2015-10-27 10:38:42 -0400

    build on fedora 21

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+docker pull andrewrothstein/docker-ansible:ubuntu_trusty
 docker build -t andrewrothstein/docker-julia:ubuntu_trusty ubuntu_trusty
+
+docker pull andrewrothstein/docker-ansible:fedora_21
+docker build -t andrewrothstein/docker-julia:fedora_21 fedora_21
