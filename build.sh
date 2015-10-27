94984f26f6bbbc87a91b0ef34d9f2db0c22554a2
Andrew Rothstein <arothste@blackrock.com>
2015-10-27 10:48:13 -0400

    include centos 7.1 build

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ docker build -t andrewrothstein/docker-julia:ubuntu_trusty ubuntu_trusty
 
 docker pull andrewrothstein/docker-ansible:fedora_21
 docker build -t andrewrothstein/docker-julia:fedora_21 fedora_21
+
+docker pull andrewrothstein/docker-ansible:centos_7.1.1503
+docker build -t andrewrothstein/docker-julia:centos_7.1.1503 centos_7.1.1503
