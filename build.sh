1e589ba4cd63b31bdcdc466eab7eb8e2e3da992f
Andrew Rothstein <arothste@blackrock.com>
2015-12-16 10:15:03 -0500

    added a ubuntu_vivid and ubuntu_wily build

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 docker pull andrewrothstein/docker-ansible:ubuntu_trusty
+docker pull andrewrothstein/docker-ansible:ubuntu_vivid
+docker pull andrewrothstein/docker-ansible:ubuntu_wily
 docker pull andrewrothstein/docker-ansible:fedora_21
 docker pull andrewrothstein/docker-ansible:centos_7.1.1503
 
 docker build -t andrewrothstein/docker-julia:ubuntu_trusty ubuntu_trusty
+docker build -t andrewrothstein/docker-julia:ubuntu_vivid ubuntu_vivid
+docker build -t andrewrothstein/docker-julia:ubuntu_wily ubuntu_wily
 docker build -t andrewrothstein/docker-julia:fedora_21 fedora_21
 docker build -t andrewrothstein/docker-julia:centos_7.1.1503 centos_7.1.1503
